421e6e2935c4488c738b34b2ef3ef64e6a240cc0
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-12 16:55:47 -0500

    List comprehension FTW

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 an in-class solution to this exercise:
 
 https://github.com/prof-rossetti/intro-to-python/blob/master/exercises/monthly-sales-reporting/README.md
+
+## Installation
+
+Fork it, clone it, nav from command line:
+
+```sh
+cd my-repo
+```
+
+## Setup
+
+Packages to install?
+
+Virtual environment?
+
+## Usage
+
+```sh
+python path/to/your/file.py
+```
--- three_charts.py
+++ three_charts.py
@@ -1,8 +1,19 @@
 import plotly
 import plotly.graph_objs as go
 
-labels = ["Oxygen", "Hydrogen", "Carbon_Dioxide", "Nitrogen"]
-values = [4500, 2500, 1053, 500]
+# INITIAL EXAMPLE
+
+#labels = ["Oxygen", "Hydrogen", "Carbon_Dioxide", "Nitrogen"]
+#values = [4500, 2500, 1053, 500]
+
+pie_data = [
+    {"company": "Company X", "market_share": 0.55},
+    {"company": "Company Y", "market_share": 0.30},
+    {"company": "Company Z", "market_share": 0.15}
+]
+
+labels = [d["company"] for d in pie_data]
+values = [d["market_share"] for d in pie_data]
 
 trace = go.Pie(labels=labels, values=values)
 
